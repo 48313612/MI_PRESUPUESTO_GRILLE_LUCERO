11d637ac3a216800b968a0de182293e1353a79f2
Android Studio <devandroid@ort>
2025-10-21 14:08:21 -0300

    css mejorado

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,10 +3,10 @@ import { NavLink } from "react-router-dom";
 function Navbar() {
   return (
     <nav className="nav">
-      <NavLink to="/" >Inicio</NavLink>
-      <NavLink to="/nuevo">Nuevo</NavLink>
-      <NavLink to="/resumen">Resumen</NavLink>
-      <NavLink to="/ajustes">Ajustes</NavLink>
+      <NavLink to="/" className={({ isActive }) => isActive ? 'active' : ''}>Inicio</NavLink>
+      <NavLink to="/nuevo" className={({ isActive }) => isActive ? 'active' : ''}>Nuevo</NavLink>
+      <NavLink to="/resumen" className={({ isActive }) => isActive ? 'active' : ''}>Resumen</NavLink>
+      <NavLink to="/ajustes" className={({ isActive }) => isActive ? 'active' : ''}>Ajustes</NavLink>
     </nav>
   );
 }
